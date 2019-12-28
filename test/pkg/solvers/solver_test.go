c198292e1e8ab7eb0637b93dc65d899f0989faae
Charles Anderson <irynnebaguma@Irynnes-MacBook-Pro.local>
2019-12-28 15:39:48 +0000

    Cleaned up tests and renamed threeSquare_test.go to nineSquare_test.go

--- test/pkg/solvers/solver_test.go
+++ test/pkg/solvers/solver_test.go
@@ -7,15 +7,6 @@ import (
 func TestCheckValid(t *testing.T) {
 	t.Skip("Skipped - implement TestCheckValid when function implemented")
 }
-
-func TestCheckVertical(t *testing.T) {
-	t.Skip("Skipped - implement TestCheckVertical when function implemented")
-}
-
-func TestCheckSquares(t *testing.T) {
-	t.Skip("Skipped - implement TestCheckSquares when function implemented")
-}
-
 func TestSolve(t *testing.T) {
 	t.Skip("Skipped - implement TestSolve when function implemented")
 }
